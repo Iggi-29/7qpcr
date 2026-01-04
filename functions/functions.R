5f6ec3561ba7238f69a36d76537330884463cf2f
Iggi-29 <ignasijs29@gmail.com>
2026-01-04 18:43:00 +0100

    added function to do  the jumps

--- functions/functions.R
+++ functions/functions.R
@@ -54,3 +54,18 @@ run_test <- function(df, value_col, group_col, test_type = "ttest") {
     wilcox.test(x ~ g)$p.value
   }
 }
+## detect frontiers
+detect_frontiers <- function(x, magnitude_jump = 2) {
+  x <- as.numeric(x)
+  
+  # calculate the order of magnitude of each value
+  orders <- floor(log10(x))
+  
+  # difference between consecutive orders
+  diff_orders <- abs(diff(orders))
+  
+  # detect jumps >= magnitude_jump
+  frontier_indices <- which(diff_orders >= magnitude_jump) + 1  # +1 because diff shifts by 1
+  
+  return(frontier_indices)
+}
